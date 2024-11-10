56a55d74e8c86c44c5f56f1a2aedb5c26c8745e0
chan <ckstj8027@naver.com>
2024-11-11 03:31:36 +0900

    feat(handler): 프로모션비활성화  핸들러 도입

---
+++ src/main/java/handler/InActivePromotionHandler.java
@@ -0,0 +1,31 @@
+package handler;
+
+
+import domain.invertory.Inventory;
+import dto.SaleDto;
+import startegy.InActivePromotionStrategy;
+
+public class InActivePromotionHandler implements PromotionHandler {
+
+
+    private final InActivePromotionStrategy promotionStrategy;
+
+
+    public InActivePromotionHandler(InActivePromotionStrategy promotionStrategy) {
+        this.promotionStrategy = promotionStrategy;
+
+    }
+
+
+    @Override
+    public SaleDto handle(String orderName, int orderQuantity, Inventory inventory) {
+        // 비활성화된 프로모션 처리
+        int promotionSaleQuantity = promotionStrategy.calculatePromotionSaleQuantity(orderQuantity, inventory);
+        int generalSaleQuantity = promotionStrategy.calculateGeneralSaleQuantity(orderQuantity, promotionSaleQuantity);
+        generalSaleQuantity = promotionSaleQuantity;
+        promotionSaleQuantity = 0;
+
+        return new SaleDto(generalSaleQuantity, promotionSaleQuantity);
+
+    }
+}
